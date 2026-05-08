b612f2a75135a94cc133945114c4c559c76f9ea7
JordanBarrera-dev <jbarcar0319@gmail.com>
2026-05-08 14:58:18 -0500

    changes

--- generationClinica/interfaces/Consultable.java
+++ generationClinica/interfaces/Consultable.java
@@ -2,15 +2,15 @@ package generationClinica.interfaces;
 
 import generationClinica.model.Medico;
 import generationClinica.model.Paciente;
+import generationClinica.model.Turno;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public interface Consultable {
 
-    List listarTurnosDelDia(LocalDate fecha);
-    List buscarPorMedico(Medico medico);
-    List buscarPorPaciente(Paciente paciente);
-
+    List<Turno> listarTurnosDelDia(LocalDate fecha);
+    List<Turno> buscarPorMedico(Medico medico);
+    List<Turno> buscarPorPaciente(Paciente paciente);
 
 }
